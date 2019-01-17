61d275d16e73440f1e7bf2962f120520e19647f2
papalexnuno <45659312+papalexnuno@users.noreply.github.com>
2019-01-17 10:46:00 +0000

    movimento

--- pap/Assets/Code/PlayerMovement.cs
+++ pap/Assets/Code/PlayerMovement.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D controller;
-    public Animator animator;
+    //public Animator animator;
 
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
+
 	void Update () {
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+        //animator.SetFloat("speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
-            animator.SetBool("jumping", true);
+            //animator.SetBool("jumping", true);
         }
         
         if (Input.GetButtonDown("Crouch"))
@@ -34,12 +35,12 @@ public class PlayerMovement : MonoBehaviour {
 
 	}
 
-    public void OnLanding()
+    /*public void OnLanding()
     {
 
         animator.SetBool("jumping", false);
 
-    }
+    }*/
 
     void FixedUpdate ()
     {
