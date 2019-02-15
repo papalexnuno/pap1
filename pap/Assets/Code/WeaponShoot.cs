cd57d3d8e82c5d2827f604f59bdb80663024f294
papalexnuno <45659312+papalexnuno@users.noreply.github.com>
2019-02-15 20:06:28 +0000

    lll

---
+++ pap/Assets/Code/WeaponShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShoot : MonoBehaviour
+{
+
+    public Transform FirePoint;
+    public GameObject BulletPreFab;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(BulletPreFab, FirePoint.position, FirePoint.rotation);
+    }
+}
