f4ec00c677092a3da2ae5b943c7932e1ac175dad
papalexnuno <45659312+papalexnuno@users.noreply.github.com>
2019-02-20 22:27:55 +0000

    prontopararelatorio

--- pap/Assets/Code/CameraFollow.cs
+++ pap/Assets/Code/CameraFollow.cs
@@ -11,8 +11,9 @@ public class CameraFollow : MonoBehaviour {
 
     public GameObject player;
 
-    public bool bounds;
+    public bool bounds;     //Variavel para criar os limites da camera
 
+    //criação da variavel para a posição minima e maxima da camera
     public Vector3 minCameraPos;
     public Vector3 maxCameraPos;
 
@@ -23,14 +24,15 @@ public class CameraFollow : MonoBehaviour {
     }
 
     void FixedUpdate()
-    {
+    {   //camera ira seguir o Player quando ele estiver-se a movimentar para x
         float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        //camera ira seguir o Player quando ele estiver-se a movimentar para y
         float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
         transform.position = new Vector3(posX, posY, transform.position.z);
 
         if (bounds)
-        {
+        {   //aplicar a área e a localização da camera para x,y e z
             transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
                 Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
                 Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
