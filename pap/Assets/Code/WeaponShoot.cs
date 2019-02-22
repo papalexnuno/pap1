2a0a10350c356ce4b11ba0de572fe1995b0160a2
papalexnuno <45659312+papalexnuno@users.noreply.github.com>
2019-02-22 15:22:24 +0000

    213

--- pap/Assets/Code/WeaponShoot.cs
+++ pap/Assets/Code/WeaponShoot.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class WeaponShoot : MonoBehaviour
 {
-
     public Transform FirePoint;
-    public GameObject BulletPreFab;
+    public int damage = 40;
+    public GameObject impactEffect;
+    public LineRenderer lineRenderer;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+            StartCoroutine(shoot());
         }
     }
 
-    void Shoot()
+    IEnumerator shoot()
     {
-        Instantiate(BulletPreFab, FirePoint.position, FirePoint.rotation);
+        RaycastHit2D hitInfo = Physics2D.Raycast(FirePoint.position, FirePoint.right);
+
+        if (hitInfo)
+        {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+
+            lineRenderer.SetPosition(0, FirePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, FirePoint.position);
+            lineRenderer.SetPosition(1, FirePoint.position + FirePoint.right * 100);
+        }
+        lineRenderer.enabled = true;
+
+        yield return new WaitForSeconds(0.04f);
+
+        lineRenderer.enabled = false;
     }
 }
