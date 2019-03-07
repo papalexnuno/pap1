4240dc0a28d3f610d67279686e1e1914df152be1
alex <alex.pereira13@live.com.pt>
2019-03-07 15:17:17 +0000

    menu já dá

---
+++ pap/Assets/Code/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour{
+
+    public void StartApp()
+    {
+        SceneManager.LoadScene("paap");
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+
+}
