c71ff6f8843dfcbc17764665b7f9d364dcad4932
alex <alex.pereira13@live.com.pt>
2019-03-21 11:30:56 +0000

    menu de pausa

---
+++ pap/Assets/Code/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool EmPausa = false;
+    public GameObject PauseMenuUi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (EmPausa)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        PauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        EmPausa = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        EmPausa = false;
+    }
+
+    public void IniMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
