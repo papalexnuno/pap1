e2a8d4cb35cdcf0095d184cb3e0aa9faf262beba
alex <alex.pereira13@live.com.pt>
2019-03-22 12:13:19 +0000

    555

--- pap/Assets/Code/EnemySpawn.cs
+++ pap/Assets/Code/EnemySpawn.cs
@@ -1,24 +1,60 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public GameObject Enemy;
-    float XPos;
-    Vector2 SitioSpawn;
-    public float TempoSpawn = 2f;
-    float ProxSpawn = 0.0f;
+    public enum SpawnState {Spawning, Waiting, Counting};
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
 
-    // Update is called once per frame
+    public Wave[] waves;
+    private int nextWave = 0;
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+    private SpawnState state = SpawnState.Counting;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;    
+    }
+
     void Update()
     {
-        if (Time.time > ProxSpawn)
+        if(waveCountdown <= 0)
         {
-            ProxSpawn = Time.time + TempoSpawn;
-            XPos = Random.Range(-38f, 34f);
-            SitioSpawn = new Vector2(XPos, transform.position.y);
-            Instantiate(Enemy, SitioSpawn, Quaternion.identity);
+            if(state != SpawnState.Spawning)
+            {
+                StartCoroutine ( SpawnWave (waves [ nextWave ] ) );
+            }
+            else
+            {
+                waveCountdown -= Time.deltaTime;
+            }
         }
+    }
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.Spawning;
+
+        for(int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            
+        }
+
+        state = SpawnState.Waiting;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Debug.Log("Spawning enemy: " + _enemy.name);
     }
 }
