dadab05669d021ca45fbd14c0c269fa635054fdd
alex <alex.pereira13@live.com.pt>
2019-03-22 14:38:31 +0000

    jhbjkh

--- pap/Assets/Code/EnemySpawn.cs
+++ pap/Assets/Code/EnemySpawn.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public enum SpawnState {Spawning, Waiting, Counting};
+    public enum SpawnState {Spawning, Waiting, Counting };   
+
     [System.Serializable]
     public class Wave
     {
@@ -15,8 +16,12 @@ public class EnemySpawn : MonoBehaviour
 
     public Wave[] waves;
     private int nextWave = 0;
+    
     public float timeBetweenWaves = 5f;
-    public float waveCountdown;
+    public float waveCountdown = 0f;
+
+    private float searchCountdown = 1f;
+
     private SpawnState state = SpawnState.Counting;
 
     void Start()
@@ -26,6 +31,18 @@ public class EnemySpawn : MonoBehaviour
 
     void Update()
     {
+        if(state == SpawnState.Waiting)
+        {
+            if (EnemyIsAlive() == false)
+            {
+                Debug.Log("Wave completed!");
+                return;
+            }else
+            {
+                return;
+            }
+        }
+
         if(waveCountdown <= 0)
         {
             if(state != SpawnState.Spawning)
@@ -38,14 +55,28 @@ public class EnemySpawn : MonoBehaviour
             }
         }
     }
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     IEnumerator SpawnWave(Wave _wave)
     {
+        Debug.Log("Spawning wave: " + _wave.name);
         state = SpawnState.Spawning;
 
         for(int i = 0; i < _wave.count; i++)
         {
             SpawnEnemy(_wave.enemy);
-            
+            yield return new WaitForSeconds(1f / _wave.rate);
         }
 
         state = SpawnState.Waiting;
@@ -56,5 +87,6 @@ public class EnemySpawn : MonoBehaviour
     void SpawnEnemy(Transform _enemy)
     {
         Debug.Log("Spawning enemy: " + _enemy.name);
+        Instantiate(_enemy, transform.position, transform.rotation);
     }
 }
