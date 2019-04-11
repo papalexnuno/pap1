c32050ce7dbefcc5cc297922f860998e4e547791
alex <alex.pereira13@live.com.pt>
2019-04-11 12:09:34 +0100

    healthbar

---
+++ pap/Assets/Code/GameHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+
+    private void Start()
+    {
+        HealthSystem healthSystem = new HealthSystem(100);
+
+        Debug.Log("Health: " + healthSystem.GetHealthPercent());
+        healthSystem.Damage(10);
+        Debug.Log("Health: " + healthSystem.GetHealthPercent());
+    }
+}
