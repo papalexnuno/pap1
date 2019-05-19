05355cba6f47d3d82dd3a6a576525edb7d9e68a5
papalexnuno <45659312+papalexnuno@users.noreply.github.com>
2019-05-19 22:10:10 +0100

    48

--- pap/Assets/Code/Weapon.cs
+++ pap/Assets/Code/Weapon.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+
     public Transform firePoint;
     public GameObject bulletPrefab;
     
+    //
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -15,6 +17,7 @@ public class Weapon : MonoBehaviour
         }
     }
 
+    //
     void Shoot()
     {
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
