3e6ea0aa9d98c347d7cd07da2606599a2479568f
papalexnuno <45659312+papalexnuno@users.noreply.github.com>
2019-05-26 19:38:46 +0100

    awf

---
+++ pap/Assets/Code/DeathMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DeathMenu : MonoBehaviour
+{
+    public void IniMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void StartApp()
+    {
+        SceneManager.LoadScene("paap");
+    }
+}
